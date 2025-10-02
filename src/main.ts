643ef7f18cc4a4eedd52e1ba8b6d8f8216cd61a1
Diego Fernando Albarracín Rodríguez <macrodiego24@yahoo.es>
2025-10-02 18:55:50 -0500

    Update console log message to reflect second change

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   app.useGlobalFilters( new RpcCustomExceptionFilter() );
 
   await app.listen(envs.port);
-  console.log('Hola Mundo - Primer');
+  console.log('Hola Mundo - Segundo cambio');
   logger.log(`Gateway running on port ${ envs.port }`);
 }
 bootstrap();
